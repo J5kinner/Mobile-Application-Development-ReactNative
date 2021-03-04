9686d1165808282eac86d7a9eff618477dcd323a
J5kinner <Jonah.lee.skinner@gmail.com>
2021-03-04 15:29:29 +1100

    Workshop 2

---
+++ week2/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>NETFLIX</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontStyle: 'italic',
+    fontSize: 45,
+    fontWeight: 'bold',
+
+  },
+});
